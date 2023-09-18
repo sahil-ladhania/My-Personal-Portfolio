cd69b7a273e8da7d0ade60e5a1eab34feecacc7a
Sahil <sahilladhania5@gmail.com>
2023-09-18 11:12:44 +0530

    Started Building Form.

--- src/app/components/FormComponent.tsx
+++ src/app/components/FormComponent.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client";
+import React, {useState} from 'react'
 import { TbPlugConnected } from "react-icons/tb";
 
 const FormComponent = () => {
+    // State Variables
+    const [name , setName] = useState('');
+    const [email , setEmail] = useState('');
+    const [message , setMessage] = useState('');
+    // Handler Functions
+    const handleSubmit = () => {
+        
+    }
     return (
         <div>
             <>
@@ -15,13 +24,13 @@ const FormComponent = () => {
                         </h1>
                         {/* Name and Email Feilds */}
                         <div className='flex justify-between mb-5'>
-                            <input className='w-2/5 h-10 p-3 rounded-md outline-none bg-slate-200 text-slate-900 focus:outline-none focus:border-teal-600 focus:ring-2 focus:ring-teal-600' type="text" name="" id="" placeholder='Aditiyaa Kumar'/>
-                            <input className='w-2/4 h-10 p-3 rounded-md outline-none bg-slate-200 text-slate-900 invalid:text-red-600 focus:outline-none focus:border-teal-600 focus:ring-2 focus:ring-teal-600' type="email" name="" id="" placeholder='baigan@gmail.com'/>
+                            <input value={name} onChange={(e) => setName(e.target.value)} className='w-2/5 h-10 p-3 rounded-md outline-none bg-slate-200 text-slate-900 focus:outline-none focus:border-teal-600 focus:ring-2 focus:ring-teal-600' type="text" name="" id="" placeholder='Aditiyaa Kumar'/>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} className='w-2/4 h-10 p-3 rounded-md outline-none bg-slate-200 text-slate-900 invalid:text-red-600 focus:outline-none focus:border-teal-600 focus:ring-2 focus:ring-teal-600' type="email" name="" id="" placeholder='baigan@gmail.com'/>
                         </div>
                         {/* Message Feild and Button */}
                         <div className='flex flex-col'>
-                            <textarea className='h-40 p-3 mb-3 rounded-md outline-none bg-slate-200 text-slate-900 focus:outline-none focus:border-teal-600 focus:ring-2 focus:ring-teal-600' name="" id="" cols={30} rows={10} placeholder='Aditiya Kumar 6 class mai padhte hai...'/>
-                            <button className='border-2 h-10 rounded-md border-transparent bg-teal-300 text-slate-600 font-semibold hover:translate-x-1'>Send Message</button>
+                            <textarea value={message} onChange={(e) => {setMessage(e.target.value)}} className='h-40 p-3 mb-3 rounded-md outline-none bg-slate-200 text-slate-900 focus:outline-none focus:border-teal-600 focus:ring-2 focus:ring-teal-600' name="" id="" cols={30} rows={10} placeholder='Aditiya Kumar 6 class mai padhte hai...'/>
+                            <button onClick={handleSubmit} className='border-2 h-10 rounded-md border-transparent bg-teal-300 text-slate-600 font-semibold hover:translate-x-1'>Send Message</button>
                         </div>
                     </form>
                 </div>
